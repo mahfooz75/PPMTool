e7fb4d95fdce3e1ff2c99a8c082aac3daacce9ca
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-01-29 08:48:25 +0530

    Custom exception for unique project identifier

--- PPMToolFullStack/src/main/java/com/iris/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/src/main/java/com/iris/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.iris.ppmtool.services;
 
 import com.iris.ppmtool.domain.Project;
+import com.iris.ppmtool.exceptions.ProjectIdException;
 import com.iris.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,8 +11,13 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdate(Project project){
-        return  projectRepository.save(project);
+    public Project saveOrUpdate(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception ex) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
     }
 
 }
