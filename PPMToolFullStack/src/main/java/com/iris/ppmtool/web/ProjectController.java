25036c5e995ebf2d9f07bbfaae056247633242b5
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-01-29 06:32:44 +0530

    Refactor project controller

--- PPMToolFullStack/src/main/java/com/iris/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/com/iris/ppmtool/web/ProjectController.java
@@ -1,6 +1,7 @@
 package com.iris.ppmtool.web;
 
 import com.iris.ppmtool.domain.Project;
+import com.iris.ppmtool.services.MapValidationErrorService;
 import com.iris.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,18 +25,17 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     @PostMapping("")
-    public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result){
+    public ResponseEntity<?> createProject(@Valid @RequestBody Project project, BindingResult result) {
 
-        if(result.hasErrors()){
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error : result.getFieldErrors()){
-                errorMap.put(error.getField(),error.getDefaultMessage());
-            }
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        ResponseEntity<?> errorMap = mapValidationErrorService.mapValidationService(result);
+        if (errorMap != null) {
+            return errorMap;
         }
-
-        Project project1=projectService.saveOrUpdate(project);
+        Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
     }
 
