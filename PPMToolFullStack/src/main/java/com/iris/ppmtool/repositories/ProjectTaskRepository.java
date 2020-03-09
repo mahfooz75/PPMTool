b5d7a3baef7c9fdc03784cf7ab8c72db30a0189c
Mahfooz Ahmad <mahfooz.lakhani@gmail.com>
2020-03-09 20:21:12 +0530

    Backlog and ProjectTask Entities

---
+++ PPMToolFullStack/src/main/java/com/iris/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.iris.ppmtool.repositories;
+
+import com.iris.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
